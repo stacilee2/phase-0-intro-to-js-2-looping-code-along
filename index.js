8ecd90b8f2048faa8c6535109fedb18b3a930670
Staci Rosales <stacileep2@gmail.com>
2022-10-03 12:44:26 -0500

    commit tree cca96b7a2121f9f962248048590b612a785be6c8

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age++) {
+   // console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+//}
+//const gifts = ["teddy bear", "drone", "doll"];
+
+//function wrapGifts(gifts) {
+    //for (let i = 0; i < gifts.length; i++) {
+        //console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        //debugger;
+   // }
+   // return gifts;
+//}
+
+//wrapGifts(gifts);
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = ["surprise"];
+
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown() {
+    let x = 10
+    while (x >= 0) {
+        console.log(x--);
+    }
+}
+
+
+
